a856b0ac3793299562e415bf5b01a02fd01d3727
nammingi <kisdki2010@gmail.com>
2021-02-19 02:00:39 +0900

    19 Feb, 0220

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./loading";
+import * as Location from "expo-location";
+import { Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+export default class extends React.Component{
+
+  getLocation = async () => {
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync();
+      console.log(location);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    return (
+      <Loading />
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
